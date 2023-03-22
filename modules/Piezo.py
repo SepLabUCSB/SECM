a5814d26df43e85b97e9a235762dac765df3903f
Brian Roehrich <bwroehrich@gmail.com>
2023-03-22 12:54:37 -0700

    add piezo control commands

--- modules/Piezo.py
+++ modules/Piezo.py
@@ -1,5 +1,6 @@
 import numpy as np
 from utils.utils import Logger
+import pyvisa
 
 
 
@@ -11,16 +12,57 @@ class Piezo(Logger):
         self.willStop = False
         self.x = 0
         self.y = 0
-        self.z = 0
+        self.z = 50
         self.starting_coords = (0,0)
-        pass
+        self._piezo_on = False
+        
+        if not self.master.TEST_MODE:
+            self.setup_piezo()
+     
+        
+    def setup_piezo(self):
+        rm = pyvisa.ResourceManager()
+        self.inst = rm.open_resource(CONTROLLER_ADDRESS)
+        
+        # Set all channels to remote control only
+        self.inst.write('setk,0,1')
+        self.inst.write('setk,1,1')
+        self.inst.write('setk,2,2')
+        
+        # Set to closed loop
+        self.inst.write('cloop,0,1')
+        self.inst.write('cloop,1,1')
+        self.inst.write('cloop,2,1')
+        
+        # Output actuator position instead of voltage
+        self.inst.write('monwpa,0,1')
+        
+        self._piezo_on = True
+        
     
+    # Read (x,y,z) from piezo
+    def measure_loc(self):
+        if not self._piezo_on:
+            return
+        location = self.inst.query('measure')
+        _, x, y, z = location.split(',')
+        
+    
+    # Return current (software) x,y,z
+    # TODO: measure every time?
     def loc(self):
         return (self.x, self.y, self.z)
     
+    
+    # Send piezo to (x,y,z)
+    # TODO: measure loc after setting?
     def goto(self, x, y, z):
+        if self._piezo_on:
+            self.inst.write(f'setall,{x},{y},{z}')
         self.x, self.y, self.z = x, y, z
         return self.loc()
+    
+    
     
     def get_xy_coords(self, length, n_points):
         # Generate ordered list of xy coordinates for a scan
@@ -61,6 +103,9 @@ class Piezo(Logger):
         
         return points, order
     
+    
+    # When zooming in to a new region, set new starting coordinates
+    # for the scan (otherwise always starts from 0,0)
     def set_new_scan_bounds(self, corners):
         # i.e. [(5, 5), (5, 44), (44, 5), (44, 44)]
         corners.sort()
@@ -69,35 +114,9 @@ class Piezo(Logger):
         print(f"starting coords: {self.starting_coords}")
         return scale
     
+    
+    # Reset to (0,0)
     def zero(self):
         self.starting_coords = (0,0)
         self.goto(0,0, self.z)
     
-
-    
-if __name__ == '__main__':
-    # grid testing
-    points = []
-    order  = []
-    coords = np.linspace(0,1,9)
-    cnt = 0
-    reverse = False
-    for j, y in reversed(list(enumerate(coords))):
-        s = ''
-        o = ''
-        if reverse:
-            for i, x in reversed(list(enumerate(coords))):
-                points.append((x,y))
-                order.append((i,j))
-                s = f'({x:.1f}, {y:.1f}) ' + s
-                o = f'{str(cnt).rjust(3)} ' + o
-                cnt += 1
-            reverse = False
-        else:
-            for i, x in enumerate(coords):
-                points.append((x,y))
-                order.append((i,j))
-                s += f'({x:.1f}, {y:.1f}) '
-                o += f'{str(cnt).rjust(3)} '
-                cnt += 1
-            reverse = True
