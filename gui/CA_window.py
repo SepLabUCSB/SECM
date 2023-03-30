d6be0347adbf9fd3c4950930cfbc6c633882e432
Brian Roehrich <bwroehrich@gmail.com>
2023-03-30 13:15:01 -0700

    first chrono window addition

---
+++ gui/CA_window.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter.ttk import *
+from functools import partial
+
+from utils.utils import focus_next_widget, run
+
+
+
+def make_CA_window(gui, master_frame):
+    
+    frame = Frame(master_frame)
+    frame.grid(column=1, row=1)
+    
+    ttk.Label(frame, text='Test').grid(column=2, row=2, sticky=(E))
+    
+    Button(frame, text='Poll ADC',
+           command=partial(run, gui.master.ADC.polling)
+           ).grid(column=2, row=8)
+        
+    return
+
+
+
