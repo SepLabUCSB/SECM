156d27c22706c3d4918233205dfaf1144cc3bc95
Brian Roehrich <bwroehrich@gmail.com>
2023-08-16 15:39:10 -0700

    explitic DataStorage import

--- src/__init__.py
+++ src/__init__.py
@@ -4,11 +4,14 @@
 
 from .modules import * # Ensures that all the modules have been loaded in their new locations *first*.
 from . import modules  # imports src/modules
+from .modules.DataStorage import *
+from .modules import DataStorage
 from .analysis import *
 from . import analysis
 from .gui import *
 from . import gui
 import sys
 sys.modules['modules'] = modules  # creates a modules entry in sys.modules
+sys.modules['modules.DataStorage'] = DataStorage
 sys.modules['analysis'] = analysis
 sys.modules['gui'] = gui
