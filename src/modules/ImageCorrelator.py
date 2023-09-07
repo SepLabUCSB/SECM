30943c40634c36afedb5350710b67a4a96df6f66
Brian Roehrich <bwroehrich@gmail.com>
2023-09-07 16:57:27 -0700

    working on image correlation

--- src/modules/ImageCorrelator.py
+++ src/modules/ImageCorrelator.py
@@ -4,6 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
+from libtiff import TIFF
 from ..utils.utils import Logger
 
 
@@ -15,14 +16,195 @@ class ImageCorrelator(Logger):
     def __init__(self, master):
         self.master = master
         self.master.register(self)
+        self.willStop = False
+        
+        self.clicked = False
         
     
     def load_image(self, file):
-        pass
+        '''
+        Make the popup and show the selected image in self.ax
+        '''
+        
+        # Open the image file
+        try:        
+            tif = TIFF.open(file)
+            img = tif.read_image()
+        except:
+            self.log(f'Error: cannot open {file}')
+            return
+        
+        # Make popup
+        self.popup = Toplevel()
+        self.topframe = Frame(self.popup)
+        self.topframe.grid(row=0, column=0)
+        self.botframe = Frame(self.popup)
+        self.botframe.grid(row=1, column=0)
+        
+        
+        # Show image
+        self.fig = plt.Figure(figsize=(5,5), dpi=150)
+        self.fig.add_subplot(111)
+        self.ax = self.fig.gca()
+        
+        
+        
+        self.image1 = self.ax.imshow(img, cmap='gray', origin='upper')
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+        for sp in ['right', 'top', 'left', 'bottom']:
+            self.ax.spines[sp].set_visible(False)
+        
+        self.original_axlim = [self.ax.get_xlim(), self.ax.get_ylim()]
+        self.rect = matplotlib.patches.Rectangle((0,0), 0, 0, fill=0,
+                                                 edgecolor='red', lw=2)
+        FigureCanvasTkAgg(self.fig, master=self.botframe
+                          ).get_tk_widget().grid(row=0, column=0)
+        
+        plt.pause(0.001)
+        
+        
+        
+        # Connect mouse clicks to draw zoom square
+        self.clickedcid = self.fig.canvas.mpl_connect('button_press_event',
+                                                      self.on_press)
+        self.releasecid = self.fig.canvas.mpl_connect('button_release_event',
+                                                      self.on_release)
+        self.dragcid    = self.fig.canvas.mpl_connect('motion_notify_event',
+                                                      self.on_drag)
+        
+        # Make other buttons in popup
+        Button(self.topframe, text='\n     Reset     \n', command=self.reset).grid(
+            row=0, column=0, sticky=(W,E), )
+        Button(self.topframe, text='\n     Draw Grid     \n', command=self.draw_grid
+               ).grid(row=0, column=1, sticky=(W,E))
+        
+        
+    def reset(self):
+        '''
+        Redraw entire original image
+        '''
+        self.ax.set_xlim(self.original_axlim[0])
+        self.ax.set_ylim(self.original_axlim[1])
+        self.loc1   = self.loc2   = (0,0)
+        self.coord1 = self.coord2 = (0,0)
+        self.draw_rect()
+        if hasattr(self, 'gridlns'):
+            for artist in self.gridlns:
+                artist.remove()
+            self.gridlns = []
+        plt.pause(0.001)
+     
+        
+    def on_press(self, event):
+        # Click. Start rectangle dragging
+        if event.inaxes != self.ax:
+            return
+        self.clicked = True
+        self.loc1   = (event.x, event.y)
+        self.coord1 = (event.xdata, event.ydata)
     
     
-    def draw_on_point(self, x, y):
-        pass
+    def on_release(self, event):
+        # Release. Zoom in to rectangle
+        self.clicked = False
+        self.zoom_to(self.coord1, self.coord2) 
+    
+    
+    def on_drag(self, event):
+        if (not self.clicked or event.inaxes != self.ax):
+            return
+        self.loc2   = (event.x, event.y)
+        self.coord2 = (event.xdata, event.ydata)
+        
+        # Force it to be a square
+        def force_square(coord1, coord2):
+            x1, y1 = coord1
+            x2, y2 = coord2
+            width  = x2 - x1
+            height = y2 - y1
+            
+            delta = min(abs(width), abs(height))
+            if delta == 0:
+                return (x2, y2)
+            
+            force_x2 = x1 + delta * width/abs(width)
+            force_y2 = y1 + delta * height/abs(height)
+            return (force_x2, force_y2)
+        
+        # self.loc2   = force_square(self.loc1, self.loc2)
+        # self.coord2 = force_square(self.coord1, self.coord2)
+        
+        self.draw_rect()
+            
+        
+    def draw_rect(self):
+        x1, y1 = self.loc1
+        x2, y2 = self.loc2
+        width  = x2 - x1
+        height = y2 - y1
+        self.rect.set_x(x1)
+        self.rect.set_y(y1)
+        self.rect.set_width(width)
+        self.rect.set_height(height)
+        
+        # Redraw with blitting
+        self.rect.set_animated(True)
+        self.fig.canvas.draw()
+        self.bg = self.fig.canvas.copy_from_bbox(self.ax.bbox)
+        
+        self.ax.draw_artist(self.rect)
+        self.fig.canvas.blit(self.ax.bbox)
+        
+        
+    def zoom_to(self, corner1, corner2):
+        if corner1 == (0,0) or corner2 == (0,0):
+            return
+        x1, y1 = corner1
+        x2, y2 = corner2
+        xlim = [x1, x2]
+        xlim.sort()
+        ylim = [y1, y2]
+        ylim.sort(reverse=True)
+        self.ax.set_xlim(xlim)
+        self.ax.set_ylim(ylim)
+        self.loc1   = self.loc2   = (0,0)
+        self.coord1 = self.coord2 = (0,0)
+        self.draw_rect()
+        plt.pause(0.001)
+    
+    
+    def draw_grid(self):
+        if hasattr(self, 'gridlns'):
+            for artist in self.gridlns:
+                artist.remove()
+            self.gridlns = []
+        plt.pause(0.001)
+        width = height = self.master.expt.length
+        n_pts = len(self.master.expt.data[0]) # points per row/ column
+        
+        xmin, xmax = self.ax.get_xlim()
+        ymin, ymax = self.ax.get_ylim()
+        
+        v_lines = [n for n in np.linspace(xmin, xmax, n_pts + 1)]
+        h_lines = [n for n in np.linspace(ymin, ymax, n_pts + 1)]
+        
+        self.gridlns = []
+        
+        for ln in v_lines:
+            self.gridlns.append(self.ax.axvline(ln, color='red', lw=0.5))
+        for ln in h_lines:
+            self.gridlns.append(self.ax.axhline(ln, color='red', lw=0.5))
+        for artist in self.gridlns:
+            self.ax.draw_artist(artist)
+        
+        self.fig.canvas.draw_idle()
+        plt.pause(0.01)
+        
+    
+    
+    # def draw_grid(self):
+    #     pass
 
 
 
