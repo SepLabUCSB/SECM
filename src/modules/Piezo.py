2e8e0bccaddc9004d68f057b9ce3fa09018fe973
Miguel Orozco <miguelorozco@ucsb.edu>
2024-09-10 09:29:57 -0700

    Revert change (Fix error when closing GUI)

--- src/modules/Piezo.py
+++ src/modules/Piezo.py
@@ -115,7 +115,6 @@ class Piezo(Logger):
         if not self._piezo_on:
             return
         self.port.write(f'{msg}\r'.encode('utf-8'))
-        time.sleep(0.2)
         # r = self.port.read_all()
         # if r:
         #     self.log(r)
